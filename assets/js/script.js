d9c04531b942b15b04b524401d53410d318ed9f1
Ben Hill <benjaminjhill@icloud.com>
2024-05-09 16:05:45 -0500

    created seach function that using fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,14 @@ const city = document.querySelector('#citySearch');
 const searchbtn = document.querySelector('#search-btn');
 const cityList = document.querySelector('.cityList');
 
+function titleCase(city) {
+    city = city.toLowerCase().split(' ');
+    for (var i = 0; i < city.length; i++) {
+        city[i] = city[i].charAt(0).toUpperCase() + city[i].slice(1); 
+    }
+    return city.join(' ');
+  }
+
 searchbtn.addEventListener('click', function (event) {
     let previousCities = localStorage.getItem('cities');
     let cities = [];
@@ -10,26 +18,29 @@ searchbtn.addEventListener('click', function (event) {
         cities = JSON.parse(previousCities);
     }
 
-    if (city.value.trim() === ''){
+    cityName = titleCase(city.value.trim());
+
+    if (cityName === ''){
         window.alert("You must enter a city");
         event.preventDefault();
         return;
-    } else if (cities.includes(city.value.trim())){
+    } else if (cities.includes(cityName)){
         window.alert("City already in already on the list. Click on the city name in the list.");
         event.preventDefault();
         city.value = '';
         return;
     } else {
-        cities.push(city.value.trim());
+        cities.push(cityName);
         localStorage.setItem('cities', JSON.stringify(cities));
         console.log(cities);
 
         let newLI = document.createElement('li');
         newLI.setAttribute('id', 'cityInList');
-        newLI.textContent = city.value.trim();
+        newLI.textContent = cityName;
         cityList.appendChild(newLI);
         event.preventDefault();
         city.value = '';
+        searchCity(cityName, APIKey);
     }
 });
 
@@ -47,6 +58,32 @@ function renderCityList() {
         cityList.appendChild(newLI);
     })
 }
+
+const displayCityName = document.querySelector('#cityName');
+const temp = document.querySelector('#temp');
+const wind = document.querySelector('#wind');
+const humid = document.querySelector('#humid');
+
+
+function searchCity(city, key) {
+    const searchedCity = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}`
+
+    fetch(searchedCity)
+    .then(function(response) {
+        if (!response.ok) {
+            alert('Network response was not ok :(');
+        }
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        //localStorage.setItem('weatherData', JSON.stringify(data));
+    })
+    .catch(function(error) {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
 
 document.addEventListener('DOMContentLoaded', function() {
     renderCityList();
