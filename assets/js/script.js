950d8a4bc96669b5e8a258c2a0156250d5beb2ac
Ben Hill <benjaminjhill@icloud.com>
2024-05-09 15:32:35 -0500

    created search eventlistener function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
 const APIKey = 'be38cbf483d90b0adc3e0667ff616deb';
+const city = document.querySelector('#citySearch');
+const searchbtn = document.querySelector('#search-btn');
+const cityList = document.querySelector('.cityList');
+
+searchbtn.addEventListener('click', function (event) {
+    let previousCities = localStorage.getItem('cities');
+    let cities = [];
+    if (previousCities !== null && previousCities !== undefined) {
+        cities = JSON.parse(previousCities);
+    }
+
+    if (city.value.trim() === ''){
+        window.alert("You must enter a city");
+        event.preventDefault();
+        return;
+    } else if (cities.includes(city.value.trim())){
+        window.alert("City already in already on the list. Click on the city name in the list.");
+        event.preventDefault();
+        city.value = '';
+        return;
+    } else {
+        cities.push(city.value.trim());
+        localStorage.setItem('cities', JSON.stringify(cities));
+        console.log(cities);
+
+        let newLI = document.createElement('li');
+        newLI.setAttribute('id', 'cityInList');
+        newLI.textContent = city.value.trim();
+        cityList.appendChild(newLI);
+        event.preventDefault();
+        city.value = '';
+    }
+});
